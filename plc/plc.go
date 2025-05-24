1c12d847db4614a2f0b9743b8748c3eed8c5abae
cpapplefamily <cpapplefamily@gmail.com>
2025-05-23 22:00:46 -0500

    Get PLC Coils API

--- plc/plc.go
+++ plc/plc.go
@@ -43,6 +43,7 @@ type Plc interface {
 	//Freezy Arena
 	SetAlternateIOStopState(input int, state bool)
 	ResetEstops()
+	GetAllCoils() ([coilCount]bool)
 }
 
 type ModbusPlc struct {
@@ -402,7 +403,7 @@ func (plc *ModbusPlc) update() {
 		}
 		plc.isHealthy = isHealthy
 	}
-
+	
 	plc.cycleCounter++
 	if plc.cycleCounter == cycleCounterMax {
 		plc.cycleCounter = 0
@@ -567,4 +568,9 @@ func (plc *ModbusPlc) ResetEstops(){
 	plc.inputs[blue1AStop] = true
 	plc.inputs[blue2AStop] = true
 	plc.inputs[blue3AStop] = true
+}
+
+// Returns the value of all PLC coils.
+func (plc *ModbusPlc) GetAllCoils() [coilCount]bool {
+    return plc.coils
 }
